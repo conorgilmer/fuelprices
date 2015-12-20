7c8f5ceb88479202a53bea19bf1e87d0b60503ae
Conor Gilmer <conor.gilmer@gmail.com>
2015-12-20 10:41:31 +0000

    fuel

--- sql/fuel.sql
+++ sql/fuel.sql
@@ -13,3 +13,27 @@ CREATE TABLE IF NOT EXISTS `fuelprices` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ;
 
 
+CREATE TABLE IF NOT EXISTS `garages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `address` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
+  `group` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `companies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+
+INSERT INTO `companies` (`id`, `name`) VALUES
+(1, 'Maxol'),
+(2, 'ESSO'),
+(3, 'Great Gas'),
+(4, 'Topaz'),
+(5, 'Applegreen'),
+(6, 'Top'),
+(7, 'EMO');
